1c2451a24a182bd994047e5461885188b4f0e733
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-04-12 13:04:17 +0100

    Add lazy loading

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import Auth from './containers/Auth/Auth';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
-import Checkout from './containers/Checkout/Checkout';
 import Layout from './hoc/Layout/Layout';
 import Logout from './containers/Auth/Logout/Logout';
-import Orders from './containers/Orders/Orders'
 
 import * as actions from './store/actions';
 
 import './App.css';
 
+// Lazy loading components
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
 
 class App extends React.Component {
 
@@ -23,7 +33,7 @@ class App extends React.Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Route path="/" exact component={BurguerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -32,9 +42,9 @@ class App extends React.Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path="/auth" component={Auth} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={asyncAuth} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurguerBuilder} />
           <Redirect to="/" />
